21913c857996e044f5a6be0d2e5427485ceaa6eb
Camilo Muñoz <jcamilomunozz@gmail.com>
2020-02-24 10:43:53 -0500

    Consuming movies API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const API_DOMAIN = 'http://www.omdbapi.com';
+const API_KEY = 'xxxxxx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gfi';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    const url = `${API_DOMAIN}/?apikey=${API_KEY}&t=interstellar`;
+    this.http.get(url).subscribe(
+      (resp) => console.log(resp)
+    );
+  }
+
 }
